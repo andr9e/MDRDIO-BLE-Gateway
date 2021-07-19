a8a08b0fe2bc048a3b173e747e4cb40aaaa795be
Andre Clarke <andr9e@gmail.com>
2021-07-19 13:47:26 -0400

    add environment monitor

--- net472/collector.py
+++ net472/collector.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import requests
 import time
 import os
 import subprocess
@@ -7,7 +8,7 @@ import paho.mqtt.client as mqtt #import the mqtt Client
 
 # start sdk console app
 subprocess.Popen(['sh',"/home/pi/bin/net472/start_sdk"])
-time.sleep(15) # wait 15 seconds for console Dongle Console app to load
+time.sleep(30) # wait 30 seconds for console Dongle Console app to load
 broker_address="localhost"
 
 # The callback for when a client receives a CONNACK response from the server.
@@ -17,11 +18,11 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a Published message is received from the server
 def on_message(client, userdata, msg):
-	print ("inside on_message function")
+	print (msg)
 	selectTagToPub(json.loads(msg.payload)["tagType"],client,msg.payload)
 
 def is_EnvironmentSensor(client,msg):
-	print ("inside is_environmentSensor")
+	print ("environmentSensor detected...")
 	timestampUTC = str(json.loads(msg)["timestampUTC"])
 	router_mac =str(json.loads(msg)["router_mac"])
 	router_lat=float(json.loads(msg)["router_lat"])
@@ -40,18 +41,6 @@ def is_EnvironmentSensor(client,msg):
 	#version=int(json.loads(msg)["version"])
 	temp=Temperature
 	print (type(temp))
-	#db=mysql.connector.connect(host="db-mdrdio-test.cei6tu7boufa.us-east-1.rds.amazonaws.com",port=3300,user="admin",passwd="hemlock2",db="modern_radio_sensors")	
-	#if (db):
-	#       print ("connection was successfull")
-                #db.close()
-	#else:
-	#       print ("connection failed")
-	#cursor = db.cursor()
-	#sql ""INSERT INTO environmentsensors (router_mac,rssi,Temperature,Humidity,deviceAddr,router_lat,router_long,VisibleLightPower,Pressure,MrapFrameCount,uvPower,router_deviceCount,router_major,router_minor,timestampUTC) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-	#data=(router_mac,rssi,Temperature,Humidity,deviceAddr,router_lat,router_long,Lux,pressure,MrapFrameCount,uvPower,router_deviceCount,router_major,router_minor,timestampUTC)
-	#cursor.execute(sql,data)
-	#db.commit()
-	#print (cursor.rowcount, "record inserted.")
 	client.publish("EnvironmentSensor/Temperature",Temperature)
 	client.publish("EnvironmentSensor/Humidity",Humidity)
 	client.publish("EnvironmentSensor/VisibleLightPower",Lux)
@@ -59,8 +48,22 @@ def is_EnvironmentSensor(client,msg):
 	#db.close()
 
 def is_SmartMoistureProbe(client, msg):
+	print (msg)
 	Index = json.loads(msg)["Index"]
 	client.publish("SmartMoistureProbe/Index", Index)
+
+def is_EnvironmentMonitor(client, msg):
+	print (msg)
+	BatteryVoltage = json.loads(msg)["BatteryVoltage"]
+	Temperature = json.loads(msg)["Temperature"]
+	Humidity =  json.loads(msg)["Humidity"]
+	MAC =  str(json.loads(msg)["deviceAddr"])
+	rssi = json.loads(msg)["rssi"]
+	client.publish("EnvironmentMonitor/Temperature", Temperature)
+	POST_URL= "https://dweet.io:443/dweet/for/"+MAC
+	POST_DATA ={"Temperature": Temperature,"Humidity": Humidity,"RSSI": rssi,"BatteryVoltage":BatteryVoltage}
+	r = requests.post (url = POST_URL,data = POST_DATA)
+	print ("responose web:"+ r.text)
 
 def selectTagToPub(tagType,client,msg):
 	tagList[tagType](client,msg)
@@ -69,13 +72,15 @@ client = mqtt.Client()
 
 tagList = {
         "EnvironmentSensor": is_EnvironmentSensor,
-        "SmartMoistureProbe": is_SmartMoistureProbe
+        "SmartMoistureProbe": is_SmartMoistureProbe,
+		"GARDTagV2": is_EnvironmentMonitor
 }
 client.on_connect = on_connect
 client.on_message = on_message
 print ("connecting to broker")
 client.connect(broker_address) #connect to broker
 print ("Publishing message to topic","console/cmd")
-postInterval_scanDuration="{\"postInterval\":\"120000\",\"scanDuration\":\"120000\"}"
+postInterval_scanDuration="{\"postInterval\":\"60000\",\"scanDuration\":\"60000\"}"
 client.publish("console/cmd",postInterval_scanDuration)
+print ("Updated Scan Duration Interval to 1 minutes")
 client.loop_forever()
